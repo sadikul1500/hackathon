7aefda5f9b27df359104eef976560460dba38100
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2019-05-03 20:15:07 +0600

    Update Certificate.java

--- Certificate.java
+++ Certificate.java
@@ -5,12 +5,17 @@ import java.io.*;
 public class Certificate {
 
     private String file = "person.txt";
-    private String cfile;
-    BufferedWriter bw = null;
-    FileWriter fw = null;
+    //public File cfile = new ;
+    //private BufferedWriter bw1 = null;
+    //private FileWriter fw1  =  null;
+    private String certificate = "";
 
-    BufferedReader br = null;
-    FileReader fr = null;
+    //BufferedWriter bw2 = null;
+    //FileWriter fw2  =  null;
+
+    //BufferedReader br = null;
+    //FileReader fr = null;
+    //private FileWriter fw_a;
 
     public Certificate()
     {
@@ -19,37 +24,60 @@ public class Certificate {
 
     public void writeCertificate(String st) throws IOException {
 
-        cfile = "certificate-" + st + ".txt";
-        SearchFile search = new SearchFile("academic.txt");
-        String str = search.SearchInFile(st);
-        printAcademicInfo(cfile, str);
+        String cfile = "certificate-" + st + ".txt";
+        SearchFile search_a = new SearchFile("academic.txt");
+        SearchFile search_p = new SearchFile("person.txt");
+        String str_a = search_a.SearchInFile(st);
+        String str_p = search_p.SearchInFile(st);
+        System.out.println(str_a);
+        System.out.println(str_p);
+        printAcademicInfo(cfile, str_a, str_p);
 
-        search = new SearchFile("person.txt");
+       /* search = new SearchFile("person.txt");
         str = search.SearchInFile(st);
-        printPersonalInfo(cfile, str);
+        printPersonalInfo(cfile, str);*/
     }
 
-    public void printAcademicInfo(String cfile, String content) throws IOException {
+    public void printAcademicInfo(String cfile, String academic, String person) throws IOException {
 
-        fw = new FileWriter(cfile,true);
-        bw = new BufferedWriter(fw);
-        bw.flush();
-        String [] words = content.split(",");
+        MyFileWriter file = new MyFileWriter(cfile);
+        //bw1 = new BufferedWriter(file);
+        //bw1.flush();
 
-        bw.write("_________________Academic Information_________________"+"\n\n");
-        bw.write("registration number: "+ words[0]+"\n");
-        bw.write("session: "+ words[1]+"\n");
-        bw.write("semester: "+ words[2]+"\n");
-        bw.write("year: "+ words[3]+"\n");
-        bw.write("cgpa: "+ words[4]+"\n");
-        bw.write("class-roll: "+ words[5]+"\n");
-        bw.write("\n\n\n");
+        /*fw2 = new FileWriter(cfile);
+        bw2 = new BufferedWriter(fw2);
+        bw2.flush();*/
 
+        String [] words_a = academic.split(",");
+        String[] words_p = person.split(",");
+
+        certificate += "                  Institute of Information Technology\n";
+        certificate += "                         University of Dhaka\n\n";
+        certificate += "This is to certify that ";
+        certificate += words_p[0]+", son/daughter of "+words_p[1]+" and "+words_p[2]+"\n\n";
+        certificate += "registration number: "+ words_a[0]+"\n";
+        certificate += "session: "+ words_a[1]+"\n";
+        certificate += "semester: "+ words_a[2]+"\n";
+        certificate += "year: "+ words_a[3]+"\n";
+        //bw1.write("cgpa: "+ words_a[4]+"\n");
+        certificate += "class-roll: "+ words_a[5]+"\n\n";
+        certificate += "was a regular student of software engineering , IIT, University of Dhaka. He/she obtained CGPA  ";
+        certificate +=  words_a[4]+" in this subject.\n";
+        certificate += "As far I know, he/she did not take part in any activity subversive of the state or of discipline.\n\n";
+        certificate += "I wish him/her every success in life.\n\n\n";
+        certificate += "Dr. Md. Shariful Islam\n";
+        certificate += "       (Director)\n";
+        certificate += "Institute of Information Technology\n";
+        certificate += "       University of Dhaka\n";
+
+        file.writeToFile(certificate, false);
+        //bw1.close();
+        //fw1.close();
     }
 
     public void printPersonalInfo(String cfile, String content)  throws IOException
     {
-        fw = new FileWriter(cfile,true);
+        /*fw = new FileWriter(cfile,true);
         bw = new BufferedWriter(fw);
         bw.flush();
 
@@ -60,7 +88,7 @@ public class Certificate {
         bw.write("mother's name: "+ words[2]);
         bw.write("nid: "+ words[3]);
         bw.write("address: "+ words[4]);
-        bw.write("\n");
+        bw.write("\n");*/
 
 
     }
