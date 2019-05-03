0ff28edd3a07cd589fad5a4d8a07bd5203e37c4b
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2019-05-03 20:29:18 +0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -9,13 +9,13 @@ public class Client {
 
         System.out.println("client is trying to connect");
 
-        Socket socket = new Socket("localhost",4567);
+        Socket socket = new Socket("localhost",4569);
 
         DataInputStream din = new DataInputStream(socket.getInputStream());
         DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str1="",str2=" ";
+        String str1="",str2=" "; // str2 - reply from server, str1 - client message
 
         while(true)
         {
@@ -24,6 +24,147 @@ public class Client {
 
             str1 = br .readLine();
             dout.writeUTF(str1);
+
+            if(str1.equals("1")) // login
+            {
+                str2 = din.readUTF();  // reg number
+                System.out.println("server says : " + str2);
+
+                str1 = br .readLine(); //reg number
+                dout.writeUTF(str1);
+
+                str2 = din.readUTF(); // valid or not
+                System.out.println("server says : " + str2);
+
+                if(str2.contains("invalid"))
+                {
+                    continue;
+                }
+
+                str2 = din.readUTF();  //account number
+                System.out.println("server says : " + str2);
+
+                str1 = br .readLine(); //acc number
+                dout.writeUTF(str1);
+
+                str2 = din.readUTF();  //password
+                System.out.println("server says : " + str2);
+
+                str1 = br .readLine();  // password
+                dout.writeUTF(str1);
+
+                /*str2 = din.readUTF();   //login sucess or not
+                System.out.println("server says : " + str2);
+
+                if(str2.contains("failed"))
+                {
+                    continue;
+                }*/
+
+                /*str2 = din.readUTF(); //choice list
+                System.out.println("server says : " + str2);*/
+
+                while(true)
+                {
+                    str2 = din.readUTF();   //login sucess or not
+                    System.out.println("server says : " + str2);
+
+                    if(str2.contains("failed"))
+                    {
+                        break;
+                    }
+
+                    str1 = br .readLine();  // take choice
+                    dout.writeUTF(str1);
+
+                    if(str1.equals(("1"))) // get certificate
+                    {
+                        str2 = din.readUTF(); //print balance
+                        System.out.println("server says : " + str2);
+
+                        str2 = din.readUTF(); //cut info
+                        System.out.println("server says : " + str2);
+
+                        if(!str2.contains("enough"))
+                        {
+                            str2 = din.readUTF(); //certificate
+                            System.out.println("server says : " + str2);
+                        }
+
+
+                    }
+
+                    else if(str1.equals("2"))  //academic info
+                    {
+                        for(int i=0;i<8;i++)
+                        {
+                            str2 = din.readUTF(); //certificate
+                            System.out.println("server says : " + str2);
+                        }
+                    }
+
+                    else if(str1.equals("3") || str2.equals("4"))
+                    {
+                        str2 = din.readUTF(); //amount:
+                        System.out.println("server says : " + str2);
+
+                        str1 = br .readLine();  // give amount
+                        dout.writeUTF(str1);
+
+                        str2 = din.readUTF(); //done or not
+                        System.out.println("server says : " + str2);
+                    }
+
+                    else if(str1.equals("5"))
+                    {
+                        str2 = din.readUTF(); //balance
+                        System.out.println("server says : " + str2);
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+
+                    /*str2 = din.readUTF();   //login sucess or not
+                    System.out.println("server says : " + str2);*/
+                }
+               /* if(str2.contains("enough"))
+                {
+                    continue;
+                }
+
+                str2 = din.readUTF();      //certificate
+                System.out.println("server says : " + str2);*/
+            }
+
+            else if(str1.equals("2")) //signup
+            {
+                for(int i=0;i<3;i++)
+                {
+                    str2 = din.readUTF();   //account info
+                    System.out.println("server says : " + str2);
+
+                    str1 = br .readLine(); //account info
+                    dout.writeUTF(str1);
+                }
+
+
+
+                str2 = din.readUTF();   //success
+                System.out.println("server says : " + str2);
+
+                str2 = din.readUTF();   //acc number
+                System.out.println("server says : " + str2);
+
+
+            }
+
+            else if (str1.equals("3"))
+            {
+                System.out.println("closing client");
+                break;
+            }
 
 
         }
